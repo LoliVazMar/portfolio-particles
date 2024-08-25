b3ad648418ee3401fdb591523febb04016e57952
LoliVazMar <lolyandrea@gmail.com>
2024-08-25 09:53:51 +0200

    Actualizaciones en proyectos

--- components/proyects-box.tsx
+++ components/proyects-box.tsx
@@ -1,80 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
 
-interface ProyectsBoxProps {
-  data: {
-    id: number;
-    title: string;
-    description: string;
-    image: string;
-    url: string;
-    urlCode?: string;
-    textButton: string;
-    textButtonCode?: string;
-    technologies: [
-      {
-        id: number;
-        title: string;
-        src: string;
-      }
-    ];
-  };
-}
-
-const ProyectsBox = (props: ProyectsBoxProps) => {
+const ProyectsBox = (props: { data: any }) => {
   const { data } = props;
-  const {
-    id,
-    title,
-    description,
-    image,
-    url,
-    urlCode,
-    textButton,
-    textButtonCode,
-    technologies,
-  } = data;
+
   return (
     <div
-      key={id}
+      key={data.id}
       className="proyect_card flex flex-col p-4 border border-teal-50 rounded-xl"
     >
       <Image
-        src={image}
-        alt={title}
+        src={data.image}
+        alt={data.title}
         width={400}
         height={400}
         className="w-full md:w-[400px] rounded-2xl h-auto md:h-[200px]"
       />
-      <h3 className="my-4 text-xl">{title}</h3>
-      <p>{description}</p>
+      <h3 className="my-4 text-xl">{data.title}</h3>
+      <p>{data.description}</p>
       <div className="proyect_card_technologies flex flex-row h-full my-4 justify-center items-center gap-8">
-        {technologies.map((item) => (
-          <Image
-            key={item.id}
-            src={item.src}
-            alt={item.title}
-            width={400}
-            height={400}
-            className="w-8"
-          />
-        ))}
+        {data.technologies.map(
+          (item: { id: number; src: string; title: string }) => (
+            <Image
+              key={item.id}
+              src={item.src}
+              alt={item.title}
+              width={400}
+              height={400}
+              className="w-8"
+            />
+          )
+        )}
       </div>
       <div className="proyect_card_buttons flex gap-5 mt-5 justify-center">
         <Link
-          href={url}
+          href={data.url}
           target="_blank"
           className="p-2 self-end h-fit transition duration-150 rounded-lg bg-secondary hover:bg-secondary/80"
         >
-          {textButton}
+          {data.textButton}
         </Link>
-        {urlCode && (
+        {data.urlCode && (
           <Link
-            href={urlCode}
+            href={data.urlCode}
             target="_blank"
             className="p-2 self-end h-fit transition duration-150 rounded-lg bg-secondary hover:bg-secondary/80"
           >
-            {textButtonCode}
+            {data.textButtonCode}
           </Link>
         )}
       </div>
